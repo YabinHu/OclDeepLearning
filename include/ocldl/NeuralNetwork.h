e8cae37534ace12d82ef9cd7b33925990982199c
Yabin Hu <yabin.hwu@gmail.com>
2012-12-19 22:32:00 +0800

    Add the class NeuralNetwork definition

---
+++ include/ocldl/NeuralNetwork.h
@@ -0,0 +1,25 @@
+//===- ocldl/NeuralNetwork.h ------------ Class NeuralNetwork ---*- C++ -*-===//
+//
+// Author: Yabin Hu (yabin.hwu@gmail.com)
+//
+//===----------------------------------------------------------------------===//
+//
+// This header defines class NeuralNetwork.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef OCLDL_NEURALNETWORK_H
+#define OCLDL_NEURALNETWORK_H
+
+namespace ocldl {
+
+class NeuralNetwork {
+private:
+
+public:
+  NeuralNetwork() { }
+  virtual ~NeuralNetwork();
+};
+
+} // End ocldl namespace
+#endif /* OCLDL_NEURALNETWORK_H */
